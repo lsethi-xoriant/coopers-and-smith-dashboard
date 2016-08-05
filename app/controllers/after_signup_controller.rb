958d58fe7303670b0e4ced3b793a6e6f81f4921e
thecookieorg <marko@thecookie.org>
2016-08-05 11:55:31 -0700

    added after signup controller

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -2,4 +2,22 @@ class AfterSignupController < ApplicationController
 	include Wicked::Wizard
 
 	steps :step_1, :step_2, :step_3
+
+	def show
+    @user = current_user
+    
+    render_wizard
+  end
+ 
+   def update
+     @user = current_user
+     @user.update_attributes(sign_up_params)
+ 
+     render_wizard @user
+    end
+    
+    def finish_wizard_path
+      # where you want to redirect signed up user path. example:
+      dashboards_index_path
+    end
 end
